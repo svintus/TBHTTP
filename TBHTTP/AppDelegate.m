d204f32835a8da942a8ef6103342c9c6b0ba370d
Marcus Osobase <marcosobase@yahoo.com>
2016-08-20 11:16:42 -0400

    Initial Commit

---
+++ TBHTTP/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  TBHTTP
+//
+//  Created by Marcus Osobase on 2016-08-20.
+//  Copyright © 2016 TunnelBear. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ TBHTTP/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TBHTTP
+//
+//  Created by Marcus Osobase on 2016-08-20.
+//  Copyright © 2016 TunnelBear. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
