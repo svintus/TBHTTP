9e1a25565b2acfbc75efe9b176c120b15f9eb30b
Marcus Osobase <marcosobase@yahoo.com>
2016-08-20 11:26:52 -0400

    Added TBURLSessionManager

---
+++ TBHTTP/TBURLSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  TBURLSessionManager.h
+//  TBHTTP
+//
+//  Created by Marcus Osobase on 2016-08-20.
+//  Copyright © 2016 TunnelBear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBURLSessionManager : NSObject
+
+@end
---
+++ TBHTTP/TBURLSessionManager.m
@@ -0,0 +1,13 @@
+//
+//  TBURLSessionManager.m
+//  TBHTTP
+//
+//  Created by Marcus Osobase on 2016-08-20.
+//  Copyright © 2016 TunnelBear. All rights reserved.
+//
+
+#import "TBURLSessionManager.h"
+
+@implementation TBURLSessionManager
+
+@end
