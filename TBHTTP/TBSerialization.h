5c0c2cf87ae41cbf87f321d0d5da7986dadd0f12
Marcus Osobase <marcosobase@yahoo.com>
2016-08-22 15:18:59 -0400

    Added TBSerialization

---
+++ TBHTTP/TBSerialization.h
@@ -0,0 +1,13 @@
+//
+//  TBSerialization.h
+//  TBHTTP
+//
+//  Created by Marcus Osobase on 2016-08-21.
+//  Copyright © 2016 TunnelBear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBSerialization : NSObject
+
+@end
---
+++ TBHTTP/TBSerialization.m
@@ -0,0 +1,13 @@
+//
+//  TBSerialization.m
+//  TBHTTP
+//
+//  Created by Marcus Osobase on 2016-08-21.
+//  Copyright © 2016 TunnelBear. All rights reserved.
+//
+
+#import "TBSerialization.h"
+
+@implementation TBSerialization
+
+@end
